c0b43a8d4ca32db4e59621cb043ae4f833db5e7e
Victor Mutuku <71630718+victormutuku@users.noreply.github.com>
2025-10-28 07:17:05 +0300

    Update download link to point to GitHub repository

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="w-screen h-screen flex flex-col items-center">
       <p className="font-medium text-5xl tracking-wide pt-50 pb-20">Mpesa Summary</p>
       <p></p>
-      <a href="../assets/apk/app-release.apk" target='_blank' download="mpesa-summary-v1.0.0.apk" rel='noreferrer' className="border py-4 px-12 rounded-lg font-medium text-lg cursor-pointer">Download</a>
+      <a href="https://github.com/victormutuku/Mpesa-Summary-Website/blob/master/src/assets/apk/app-release.apk" target='_blank' download="mpesa-summary-v1.0.0.apk" rel='noreferrer' className="border py-4 px-12 rounded-lg font-medium text-lg cursor-pointer">Download</a>
     </div>
   );
 }
