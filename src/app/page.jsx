e0bfa765317f5d6287c95a239c902c3e070541a6
Victor Mutuku <victin250@gmail.com>
2025-10-28 11:48:37 +0300

    Added file download name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="w-screen h-screen flex flex-col items-center">
       <p className="font-medium text-5xl tracking-wide pt-50 pb-20">Expenditure Summary</p>
       <p></p>
-      <a href="https://github.com/victormutuku/Mpesa-Summary-Website/releases/download/v1.0.0/app-release.apk" className="border py-4 px-12 rounded-lg font-medium text-lg cursor-pointer">Download</a>
+      <a href="https://github.com/victormutuku/Mpesa-Summary-Website/releases/download/v1.0.0/app-release.apk" download="expenditure-summary-v1.0.0.apk" className="border py-4 px-12 rounded-lg font-medium text-lg cursor-pointer">Download</a>
     </div>
   );
 }
