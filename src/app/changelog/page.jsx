796dbc75f1b62998643eeb7bb903ed37c76739b7
Victor Mutuku <victin250@gmail.com>
2025-11-26 15:10:48 +0300

    Fixed issue with width and mapping items

--- src/app/changelog/page.jsx
+++ src/app/changelog/page.jsx
@@ -9,15 +9,15 @@ export default function ChangeLog() {
         <div className="h-screen flex flex-col items-center pt-20 px-5 not-dark:bg-magnolia-50 dark:bg-magnolia-950 not-dark:text-magnolia-950 dark:text-magnolia-50">
             <HeaderLinks active='changelog' />
             <p className='text-2xl font-medium lg:px-20 mb-8'>ChangeLog</p>
-            <div className="lg:px-20">
-                {notesMap.map((item) => {
+            <div className="lg:px-20 max-w-[900px]">
+                {notesMap.map((item, index) => {
                     return item.value.length !== 0 ?
-                        <>
+                        <div key={index}>
                             <p key={item.key} className="text-xl font-semibold mb-4">{item.key}</p>
                             <ul className='mb-5'>
                                 {item.value.map((value, index) => <li key={index} className='list-disc list-inside'>{value}</li>)}
                             </ul>
-                        </> : <></>
+                        </div> : <></>
                 })}
             </div>
         </div>
