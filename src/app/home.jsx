62090d01b6cdd025225aa96639f9ffda0130860b
Victor Mutuku <victin250@gmail.com>
2026-06-08 13:38:58 +0300

    uncommented play store button

--- src/app/home.jsx
+++ src/app/home.jsx
@@ -7,7 +7,7 @@ import { latestVersion, arm64Url } from "./services/FirebaseConnection"
 
 
 export default function Home() {
-  const playstoreUrl = "https://play.google.com/store/apps/details?id=com.victor.mpesa_message";
+  const playstoreUrl = "https://play.google.com/store/apps/details?id=com.victor.spend_analysis";
   
   const { theme } = useTheme();
 
@@ -16,11 +16,11 @@ export default function Home() {
       <Head>
         <link rel="icon" href={theme === 'light' ? "/images/icon.png" : "/images/icon-dark.png"} sizes="any" />
       </Head>
-      <div className="flex flex-col items-center justify-center px-5 not-dark:bg-magnolia-50 dark:bg-magnolia-950 not-dark:text-magnolia-950 dark:text-magnolia-50">
+      <div className="flex flex-col items-center justify-center px-5">
         <HeaderLinks active="home" />
-        <div className="pt-10 max-w-[900px] flex flex-col items-center pb-10">
-          <Image src={theme === 'light' ? "/images/icon.png" : "/images/icon-dark.png"} width={150} height={150} alt="App Icon" />
-          <div className="-mt-3 pb-10 flex flex-col items-center">
+        <div className="pt-10 max-w-225 flex flex-col items-center pb-10">
+          <Image src={"/images/icon.png"} width={150} height={150} alt="App Icon" />
+          <div className="mt-3 pb-10 flex flex-col items-center">
             <p className="font-medium text-5xl tracking-wide">SpendAnalysis</p>
           </div>
           <div className="text-center tracking-wide text-xl font-light">
@@ -28,7 +28,7 @@ export default function Home() {
             <p className="mb-2">The summaries assist in financial planning and budgeting, as knowing you monthly expenditure assists in forecasting the next.</p>
             <p className="mb-10">The summaries generated are as accurate as your Mpesa messages are. This means that any deleted messages are not accounted for.</p>
           </div>
-          <p className="mb-5 text-2xl font-medium">Setup Instructions</p>
+          {/* <p className="mb-5 text-2xl font-medium">Setup Instructions</p>
           <div className="text-xl font-light">
             <ol className="mb-5 pl-5">
               <li className="mb-2 list-decimal">Download the application by clicking the download button below.</li>
@@ -49,25 +49,18 @@ export default function Home() {
               </ul>
               <li className="mb-2 list-decimal">Install the application. You might be requested to enable &quot;Install unknown apps&quot;. Enable this. This setting is just a security measure to preventing installing apps that are not from the Play Store</li>
             </ol>
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-col items-center justify-center pb-5">
           <div className="mb-10">
-            <a href={arm64Url}>
-              <div className="w-58 flex gap-5 justify-center items-center border-2 my-5 py-4 pl-6 pr-8 rounded-lg font-medium text-lg cursor-pointer">
-                <p>Download {latestVersion}</p>
+            <a href={playstoreUrl} target="_blank" rel="noreferrer">
+              <div className=" flex gap-5 justify-center items-center border-2 my-5 py-4 pl-6 pr-8 rounded-lg font-medium text-lg cursor-pointer whitespace-nowrap">
+                <p>Download on Play Store</p>
               </div>
             </a>
           </div>
-          {/* <div className="mb-10">
-            <a href={playstoreUrl} target="_blank" rel="noreferrer">
-              <div className=" flex gap-5 justify-center items-center border-2 my-5 py-4 pl-6 pr-8 rounded-lg font-medium text-lg cursor-pointer whitespace-nowrap">
-                <p>Download {latestVersion} on Play Store</p>
-              </div>
-            </a>
-          </div> */}
           <p className="mb-5 text-2xl font-medium">Frequently Asked Questions</p>
-          <div className="max-w-[900px] flex flex-col pb-10">
+          <div className="max-w-225 flex flex-col pb-10">
             <ol className="w-full">
               <li className="list-decimal list-inside text-xl font-medium mb-1">Does the app access my M-pesa Pin?</li>
               <p className="text-lg mb-5">No. It does not.</p>
